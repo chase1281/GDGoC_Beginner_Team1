6c3a21a609d68b1de38ace17c2efcd7a29eb9681
YEJIN-HB <yejin1228@pukyong.ac.kr>
2026-01-16 02:37:13 +0900

    신청정원 Board entity,errorCode수정

--- BE/src/main/java/com/example/StudyBoard/exception/ErrorCode.java
+++ BE/src/main/java/com/example/StudyBoard/exception/ErrorCode.java
@@ -29,7 +29,8 @@ public enum ErrorCode {
     //신청
     DUPLICATED_APPLIED(HttpStatus.BAD_REQUEST, "-400", "이미 신청한 게시글입니다."),
     SELF_APPLICATION(HttpStatus.BAD_REQUEST, "-401", "본인의 게시글에는 신청할 수 없습니다."),
-    APPLICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "-402", "신청 내역을 찾을 수 없습니다.")
+    APPLICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "-402", "신청 내역을 찾을 수 없습니다."),
+    CAPACITY_EXCEEDED(HttpStatus.BAD_REQUEST, "-403", "모집 정원이 초과되었습니다.")
     ;
 
     private final HttpStatus httpStatus;
